dc595862338373ae38c99a6deea54decaeac9e27
Jon Lareau <jonlareau@gmail.com>
2015-03-08 16:16:19 -0400

    Updated version string to v1.0.9

--- src/com/wwrkds/CDBabySalesDataCrawlerUI.java
+++ src/com/wwrkds/CDBabySalesDataCrawlerUI.java
@@ -114,7 +114,7 @@ public class CDBabySalesDataCrawlerUI {
 			ftpUserField, ftpPassField, ftpDirField, startPageField,
 			ftpServerField, ftpPortField;
 
-	private static String version = "v1.0.8";
+	private static String version = "v1.0.9";
 
 	private static void getGUIFieldData() {
 
